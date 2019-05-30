ab3be6659256fc8dbf4dbc9868663c2c878ba541
David Philpott <david.philpott@sainsburys.co.uk>
2019-05-30 09:06:10 +0100

    refactored code and replaced whoami auth test

--- balance.py
+++ balance.py
@@ -39,7 +39,10 @@ def write_tokens(token_file_path: str, tokens_to_write: dict) -> None:
     return
 
 
-def refresh_auth_token(api_client, client_id, client_secret, refresh_token):
+def refresh_auth_token(api_client, token_set):
+    client_id = token_set['client_id']
+    client_secret = token_set['client_secret']
+    refresh_token = token_set['refresh_token']
     api_url = 'https://api.monzo.com/oauth2/token'
     refresh_params = {'grant_type': 'refresh_token',
                       'client_id': client_id,
@@ -48,60 +51,33 @@ def refresh_auth_token(api_client, client_id, client_secret, refresh_token):
 
     r = api_client.post(api_url=api_url, api_parameters=refresh_params)
 
-    new_tokens = {'access_token': json.loads(r)['access_token'],
+    new_tokens = {'access_key': json.loads(r)['access_token'],
                   'refresh_token': json.loads(r)['refresh_token']}
-    print(new_tokens)
     return new_tokens
 
 
-#TODO: Replace this with a useful access tester (e.g. whoami) which works. Currently this is borken becuase its for exhanging oauth permission for access token, but im trying to give it an access token
-def test_access(api_client, client_id, client_secret, access_token):
-    api_url = 'https://api.monzo.com/oauth2/token'
-    parameters = {'grant_type': 'authorization_code',
-                  'client_id': client_id,
-                  'client_secret': client_secret,
-                  'redirect_uri': 'http://localhost:2020',
-                  'code': access_token}
-    r = api_client.post(api_url=api_url, api_parameters=parameters)
-    result_test = r.text
-    print (result_test)
-    return result_test
+def test_valid_access_key(monzo_client):
+    access_test = monzo_client.whoami()
+    return access_test['authenticated']
 
-
-api_client = APIClient()
 
 token_set = read_tokens(token_file_path=TOKEN_FILE_PATH)
-client_id = token_set['client_id']
-client_secret = token_set['client_secret']
-refresh_token = token_set['refresh_token']
-access_token = token_set['access_token']
+access_key = token_set['access_key']
 
-print(token_set)
-result_test = test_access(api_client=api_client,
-                          client_id=client_id,
-                          client_secret=client_secret,
-                          access_token=access_token)
-response_dict = json.loads(result_test)
-if 'error' in response_dict.keys():
-    if response_dict['code'] == 'unauthorized.bad_authorization_code.expired':
-        new_tokens = refresh_auth_token(api_client=api_client,
-                                        client_id=client_id,
-                                        client_secret=client_secret,
-                                        refresh_token=refresh_token)
-        refresh_token = new_tokens['refresh_token']
-        access_token = new_tokens['access_token']
+monzo = Monzo(access_key)
+if test_valid_access_key(monzo_client=monzo) is False:
+    api_client = APIClient()
+    new_tokens = refresh_auth_token(api_client=api_client,
+                                    token_set=token_set)
+    new_token_set = {'client_id': token_set['client_id'],
+                     'client_secret': token_set['client_secret'],
+                     'refresh_token': new_tokens['refresh_token'],
+                     'access_key': new_tokens['access_key']}
 
-        new_token_set = {'client_id': client_id,
-                         'client_secret': client_secret,
-                         'refresh_token': refresh_token,
-                         'access_token': access_token}
+    write_tokens(TOKEN_FILE_PATH, tokens_to_write=new_token_set)
+    monzo = Monzo(access_key)
 
-        write_tokens(TOKEN_FILE_PATH, tokens_to_write=new_token_set)
-
-print(response_dict)
-
-client = Monzo(access_token)
-account_list = client.get_accounts()['accounts']
+account_list = monzo.get_accounts()['accounts']
 for account in account_list:
     if account['closed'] is False:
         account_id = account['id']
@@ -109,13 +85,13 @@ for account in account_list:
 
 print(account_id)
 
-balance = client.get_balance(account_id) # Get your balance object
+balance = monzo.get_balance(account_id) # Get your balance object
 print(balance['balance']) # 100000000000
 print(balance['currency']) # GBP
 print(balance['spend_today']) # 2000
 
-transactions = client.get_transactions(account_id)
+transactions = monzo.get_transactions(account_id)
 print(transactions)
 
-pots = client.get_pots()['pots']
+pots = monzo.get_pots()['pots']
 print(pots)
