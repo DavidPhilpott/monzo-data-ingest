5ace08f83b1d2d9b4e2867a25dea781ac51cfb84
David Philpott <DavidJohnPhilpott@outlook.com>
2019-06-26 21:23:16 +0100

    Moved sns sub into sqs definition

--- terraform/sns-sub-to-core.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_sns_topic_subscription" "sub-core-sns-to-monzo-work-entry-queue" {
-  topic_arn = "arn:aws:sns:us-west-2:${aws_account_id}:core-message-inbox"
-  protocol  = "sqs"
-  endpoint  = aws_sqs_queue.work_entry_queue.arn
-}
