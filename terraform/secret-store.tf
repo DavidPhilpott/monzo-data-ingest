358b8224f12630ee1364515835fecfd49740f642
David Philpott <DavidJohnPhilpott@outlook.com>
2019-06-29 15:43:23 +0100

    Added parameter store spaces for secrets

---
+++ terraform/secret-store.tf
@@ -0,0 +1,72 @@
+resource "aws_ssm_parameter" "client_id" {
+  name        = "monzo-data-ingest-client-id"
+  description = "Client ID of the Monzo app which will be used to access the API."
+  type        = "String"
+  value       = "xyz"
+  
+  tags = merge(
+    {
+      "Name" = "monzo-data-ingest-client-id"
+    },
+    local.common_tags
+  )
+}
+
+resource "aws_ssm_parameter" "redirect_uri" {
+  name        = "monzo-data-ingest-redirect-uri"
+  description = "Redirect URI that the monzo app is allowed to feed back to."
+  type        = "String"
+  value       = "xyz"
+  
+  tags = merge(
+    {
+      "Name" = "monzo-data-ingest-redirect-uri"
+    },
+    local.common_tags
+  )
+}
+
+resource "aws_ssm_parameter" "secret_id" {
+  name        = "monzo-data-ingest-client-secret-id"
+  description = "Secret ID of the Monzo app which will be used to access the API."
+  type        = "SecureString"
+  value       = "xyz"
+
+  tags = merge(
+    {
+      "Name" = "monzo-data-ingest-client-secret-id"
+    },
+    local.common_tags
+  )
+
+}
+
+resource "aws_ssm_parameter" "access_key" {
+  name        = "monzo-data-ingest-access-key"
+  description = "Access key used to request from the API."
+  type        = "SecureString"
+  value       = "xyz"
+
+  tags = merge(
+    {
+      "Name" = "monzo-data-ingest-access-key"
+    },
+    local.common_tags
+  )
+
+}
+
+resource "aws_ssm_parameter" "refresh_token" {
+  name        = "monzo-data-ingest-refresh-token"
+  description = "Refresh token used to renew the Monzo access key."
+  type        = "SecureString"
+  value       = "xyz"
+
+  tags = merge(
+    {
+      "Name" = "monzo-data-ingest-refresh-token"
+    },
+    local.common_tags
+  )
+
+}
