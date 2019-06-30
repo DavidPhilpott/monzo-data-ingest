f2e12a97169a0a93cfafead70aca0bd01ee4789f
David Philpott <DavidJohnPhilpott@outlook.com>
2019-06-30 19:44:51 +0100

    Added logging permissions to lambda

--- terraform/lambda-check-valid-auth-tokens.tf
+++ terraform/lambda-check-valid-auth-tokens.tf
@@ -51,7 +51,7 @@ EOF
 
 ## EC2 Access Policy
 
-data "aws_iam_policy_document" "monzo_lambda_ec2_policy_document" {
+data "aws_iam_policy_document" "monzo_lambda_core_policy_document" {
   statement {
     actions = [
       "ec2:CreateNetworkInterface",
@@ -63,11 +63,22 @@ data "aws_iam_policy_document" "monzo_lambda_ec2_policy_document" {
       "*"
     ]
   } 
+
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+    resources = [
+      "arn:aws:logs:${var.region}:${var.aws_account_id}:*"
+    ]
+}
 }
 
-resource "aws_iam_role_policy" "monzo_lambda_ec2_policy" {
+resource "aws_iam_role_policy" "monzo_lambda_core_policy" {
   role   = aws_iam_role.iam_role_check_valid_auth_tokens_lambda.id
-  policy = data.aws_iam_policy_document.monzo_lambda_ec2_policy_document.json
+  policy = data.aws_iam_policy_document.monzo_lambda_core_policy_document.json
 }
 
 ## Other Access Policy
