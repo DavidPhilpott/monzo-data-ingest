6278c96092da36954d13e84278a43fc58e777805
David Philpott <DavidJohnPhilpott@outlook.com>
2019-07-03 20:36:02 +0100

    Created lambda for refreshing auth tokens, and renamed other lambda

---
+++ terraform/sf-lambda-check-valid-auth-tokens.tf
@@ -0,0 +1,25 @@
+resource "aws_lambda_function" "check_valid_auth_tokens" {
+  function_name = "monzo-data-ingest-check-valid-auth-tokens"
+  description   = "Tests if Monzo access key is still valid."
+  role          = aws_iam_role.iam_role_lambdas.arn
+
+  runtime           = "python3.7"
+  filename          = "../app/app.zip"
+  source_code_hash  = "${filebase64sha256("../app/app.zip")}"
+  handler           = "test_authorisation.main"
+  layers            = [aws_lambda_layer_version.monzo_requirements_lambda_layer.arn]
+
+  environment {
+    variables = {
+      access_key_parameter = aws_ssm_parameter.access_key.name,
+      logging_level = var.logging_level,
+    }
+  }
+
+  tags = merge(
+    {
+      "Name" = "monzo-data-ingest-check-valid-auth-tokens"
+    },
+    local.common_tags
+  )
+}
--- terraform/sf-lambda-iam.tf
+++ terraform/sf-lambda-iam.tf
@@ -1,35 +1,5 @@
-resource "aws_lambda_function" "check_valid_auth_tokens" {
-  function_name = "monzo-data-ingest-check-valid-auth-tokens"
-  description   = "Tests if Monzo access key is still valid."
-  role          = aws_iam_role.iam_role_check_valid_auth_tokens_lambda.arn
-
-  runtime           = "python3.7"
-  filename          = "../app/app.zip"
-  source_code_hash  = "${filebase64sha256("../app/app.zip")}"
-  handler           = "test_authorisation.main"
-  layers            = [aws_lambda_layer_version.monzo_requirements_lambda_layer.arn]
-
-  environment {
-    variables = {
-      client_id_parameter = aws_ssm_parameter.client_id.name,
-      client_secret_id_parameter = aws_ssm_parameter.client_secret_id.name,
-      redirect_uri_parameter = aws_ssm_parameter.redirect_uri.name,
-      access_key_parameter = aws_ssm_parameter.access_key.name,
-      refresh_token_parameter = aws_ssm_parameter.refresh_token.name,
-      logging_level = var.logging_level,
-    }
-  }
-
-  tags = merge(
-    {
-      "Name" = "monzo-data-ingest-check-valid-auth-tokens"
-    },
-    local.common_tags
-  )
-}
-
-resource "aws_iam_role" "iam_role_check_valid_auth_tokens_lambda" {
-  name = "lambda-role-monzo-data-ingest-check-valid-auth-tokens"
+resource "aws_iam_role" "iam_role_lambdas" {
+  name = "lambda-role-monzo-data-ingest"
 
   assume_role_policy = <<EOF
 {
