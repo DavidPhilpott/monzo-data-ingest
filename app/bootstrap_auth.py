5ca9c60858df30a7c73639c129997f2723186c17
David Philpott <david.philpott@sainsburys.co.uk>
2019-07-09 09:41:37 +0100

    Added name of thread to logger formatting

--- app/bootstrap_auth.py
+++ app/bootstrap_auth.py
@@ -20,7 +20,7 @@ def set_logger_level(logger_to_set):
 def set_logger_format(logger_to_format):
     """Set logger output format to a hardcoded version."""
     print("Setting logger formatting")
-    log_format = logging.Formatter(fmt='%(asctime)s | %(levelname)s | %(message)s',
+    log_format = logging.Formatter(fmt='%(asctime)s | %(name)s | %(levelname)s | %(message)s',
                                    datefmt='%d-%b-%y %H:%M:%S')
     log_handler = logging.StreamHandler()
     log_handler.setFormatter(log_format)
