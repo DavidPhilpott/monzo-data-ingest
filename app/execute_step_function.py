04b11a25cbd31de107ba532f624df7587212e0af
David Philpott <DavidJohnPhilpott@outlook.com>
2019-08-25 11:26:04 +0100

    Added infra for step function executor lambda

---
+++ app/execute_step_function.py
@@ -0,0 +1,67 @@
+import boto3
+import os
+import logging
+import requests
+import json
+
+
+def set_logger_level(logger_to_set):
+    """Get logging_level from environment and use to set the logging level."""
+    logging_level = os.getenv('logging_level', 'NONE').upper()
+    if logging_level is 'NONE':
+        print("No logging_level environmental variable found. Defaulting to 'DEBUG'.")
+        logging_level = 'DEBUG'
+    else:
+        print("Setting logger to level: %s." % logging_level)
+    logging_level_name = logging.getLevelName(logging_level)
+    logger_to_set.setLevel(logging_level_name)
+    return
+
+
+def set_logger_format(logger_to_format):
+    """Set logger output format to a hardcoded version."""
+    print("Setting logger formatting")
+    log_format = logging.Formatter(fmt='%(asctime)s | %(name)s | %(levelname)s | %(message)s',
+                                   datefmt='%d-%b-%y %H:%M:%S')
+    log_handler = logging.StreamHandler()
+    log_handler.setFormatter(log_format)
+    logger_to_format.addHandler(log_handler)
+    return
+
+
+def get_ssm_parameter_value(parameter_name):
+    """Make a get request to SSM for the given parameter and return un-encrypted value"""
+    logger.info("Seeking SSM value for environmental variable '%s'." % parameter_name)
+    try:
+        parameter_env = os.getenv(parameter_name, None)
+        if parameter_env is not None:
+            logger.debug("Found environmental variable. Associated value is set to '%s'" % parameter_env)
+        else:
+            raise ValueError("Could not find value for environmental variable '%s'" % parameter_name)
+    except ValueError as e:
+        logger.exception(e, exc_info=False)
+        raise e
+    logger.debug("Creating SSM client.")
+    ssm_client = boto3.client('ssm')
+    logger.debug("Requesting un-encrypted parameter information for '%s'." % parameter_env)
+    parameter_info = ssm_client.get_parameter(Name=parameter_env, WithDecryption=True)
+    logger.debug("Returned parameter_info dictionary. Seeking ['Parameter']['Value'].")
+    parameter_value = parameter_info['Parameter']['Value']
+    logger.info("Value found. Returning...")
+    return parameter_value
+
+def main(event, context):
+    print("-- Instantiating logger --")
+    global logger
+    logger = logging.getLogger(__name__)
+    logger.propagate = False
+    set_logger_level(logger)
+    set_logger_format(logger)
+
+    logger.info("-- Getting Parameter Values --")
+    sns_topic_arn = get_ssm_parameter_value(parameter_name='target_sns_arn_parameter')
+    logger.info("Finished getting parameter values.")
+
+    logger.info("-- Placeholder --")
+    logger.info("Function finished.")
+    return
---
+++ terraform/step-function-executor.tf
@@ -0,0 +1,36 @@
+//Creates a Lambda and cront job that push job notifciations onto the core SNS to trigger Monzo jobs
+
+resource "aws_lambda_function" "step_function_executor_lambda" {
+  function_name = "monzo-data-ingest-step-function-executor"
+  description   = "Starts execution of Monzo step function based on incoming SQS items."
+  role          = aws_iam_role.iam_role_lambdas.arn
+
+  runtime           = "python3.7"
+  filename          = "../app/app.zip"
+  source_code_hash  = "${filebase64sha256("../app/app.zip")}"
+  handler           = "execute_step_function.main"
+  layers            = [aws_lambda_layer_version.monzo_requirements_lambda_layer.arn]
+
+  timeout = var.lambda_timeout
+
+  environment {
+    variables = {
+      target_step_function_endpoint = "!!!Placeholder",
+      logging_level = var.logging_level,
+    }
+  }
+
+  tags = merge(
+    {
+      "Name" = "monzo-data-ingest-job-trigger"
+    },
+    local.common_tags
+  ) 
+}
+
+resource "aws_lambda_event_source_mapping" "event_source_mapping" {
+  event_source_arn = aws_sqs_queue.work_entry_queue.arn
+  enabled          = true
+  function_name    = aws_lambda_function.step_function_executor_lambda.arn
+  batch_size       = 1
+}
