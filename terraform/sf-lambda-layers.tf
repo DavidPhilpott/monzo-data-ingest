8c8fb3aaab9920301f9e7e6a1b7c8ef001f7bbf3
David Philpott <DavidJohnPhilpott@outlook.com>
2019-08-27 21:29:32 +0100

    Re-organised and renamed .tf's

--- terraform/sf-lambda-layers.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_lambda_layer_version" "monzo_requirements_lambda_layer" {
-  filename   = "../app/requirements.zip"
-  source_code_hash = "${filebase64sha256("../app/requirements.zip")}"
-  layer_name = "monzo-data-ingest-requirements-lambda-layer"
-
-  description = "Contains the library requirements for running Monzo Data Ingest lambda code."
-
-  compatible_runtimes = ["python3.7"]
-}
