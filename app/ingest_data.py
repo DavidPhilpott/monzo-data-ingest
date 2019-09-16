a341a56dd325ff66e26d22ce97668d4b376cc520
David Philpott <DavidJohnPhilpott@outlook.com>
2019-09-16 21:48:57 +0100

    Comment out most of the request due to API change causing failures

--- app/ingest_data.py
+++ app/ingest_data.py
@@ -22,21 +22,21 @@ def main(event, context):
     account_list = monzo_client.get_accounts()['accounts']
     print("Full account list")
     print(account_list)
-    for account in account_list:
-        account_id = account['id']
-        print('id: %s, closed: %s' %(account['id'], account['closed']))
-        balance = monzo_client.get_balance(account_id) # Get your balance object
-        print("Balance:")
-        print(balance['balance']) # 100000000000
-        print("Currency:")
-        print(balance['currency']) # GBP
-        print("Spend Today:")
-        print(balance['spend_today']) # 2000
-        print("Transactions:")
-        transactions = monzo_client.get_transactions(account_id)
-        print(transactions)
+    #for account in account_list:
+    #    account_id = account['id']
+    #    print('id: %s, closed: %s' %(account['id'], account['closed']))
+    #    balance = monzo_client.get_balance(account_id) # Get your balance object
+    #    print("Balance:")
+    #    print(balance['balance']) # 100000000000
+    #    print("Currency:")
+    #    print(balance['currency']) # GBP
+    #    print("Spend Today:")
+    #    print(balance['spend_today']) # 2000
+    #    print("Transactions:")
+    #    transactions = monzo_client.get_transactions(account_id)
+    #    print(transactions)
 
-    pots = monzo_client.get_pots()['pots']
-    print("Pots:")
-    print(pots)
+    #pots = monzo_client.get_pots()['pots']
+    #print("Pots:")
+    #print(pots)
     return
